52cf18550ff51541ccba8cfe32553476d3b6613b
joshuaAjack <mynameisjoshuajack@gmail.com>
2020-06-28 18:58:47 -0700

    user files done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,18 @@
 require './config/environment'
+  # register Sinatra::ActiveRecordExtension
 
 class ApplicationController < Sinatra::Base
-
+   register Sinatra::ActiveRecordExtension
+ 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "TResource"
   end
-
+    
   get "/" do
-    erb :welcome
+    erb :'/user/login'
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,50 @@
 class UserController < ApplicationController
+
+    get '/signup' do 
+        erb :'/users/signup'
+    end
+
+    post '/signup' do 
+        @users = Users.new(:email => params[:email],:password => params[:password],:name => params[:name])
+		if @users.save
+            redirect '/homepage'
+        else 
+            erb :'/users/signup'
+        end
+      end
+
+      get '/user' do 
+        erb :'/users/homepage'
+      end
+
+      post '/user' do 
+        erb :'/users/homepage'
+      end
+
+
+    get '/login' do 
+        erb :'/users/login'
+    end
+
+    post "/login" do
+		users = Users.find_by(:username => params[:username])
+
+			if users && users.authenticate(params[:password])
+			  session[:users_id] = users.id
+			  redirect '/homepage'
+			else
+			  redirect '/login'
+			end
+		end
+
+
+    get '/homepage' do 
+        erb :'/users/homepage'
+    end
+
+    get '/logout' do 
+        session.clear 
+        redirect '/users/login'
+    end
+  end
 end
